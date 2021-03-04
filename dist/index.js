7e764485117bcfe8152fda0fa976dcd23db0ca94
bkwhite <bleywhite@gmail.com>
2021-03-04 14:45:51 -0500

    [action] try to create a new check

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -7,9 +8,26 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = __importDefault(require("@actions/core"));
+const github_1 = __importDefault(require("@actions/github"));
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
-        console.log("Hello, world!");
+        const GITHUB_TOKEN = core_1.default.getInput('GITHUB_TOKEN');
+        const octokit = github_1.default.getOctokit(GITHUB_TOKEN);
+        const { context } = github_1.default;
+        const { repository } = context.payload;
+        yield octokit.checks.create({
+            owner: repository.owner.login,
+            repo: repository.full_name,
+            name: 'Cypress Check',
+            head_sha: context.sha,
+            details_url: "https://www.soomolearning.com/",
+            conclusion: 'neutral'
+        });
     });
 }
 run();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
+import core from '@actions/core'
+import github from '@actions/github'
+
 async function run() {
-    console.log("Hello, world!")
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
+    const octokit = github.getOctokit(GITHUB_TOKEN)
+
+    const { context } = github
+
+    const { repository } = context.payload
+
+    await octokit.checks.create({
+        owner: repository.owner.login,
+        repo: repository.full_name,
+        name: 'Cypress Check',
+        head_sha: context.sha,
+        details_url: "https://www.soomolearning.com/",
+        conclusion: 'neutral'
+    })
 }
 
-run();
+run()
